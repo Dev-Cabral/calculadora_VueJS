b227ed8b7904c101e7a0e85a9e005acd630f23a2
Ronario Cabral <ronariolimacabral@gmail.com>
2023-05-30 22:58:04 -0300

    commit initial

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,77 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from 'vue';
+
+const calcular = reactive({
+  campoA: '',
+  campoB: '',
+  calcularTotal: 'soma',
+  
+})
+
+
+const getSomarNumeros = () => {
+  const { campoA, campoB } = calcular;
+  return Number(campoA) + Number(campoB);
+}
+
+const getSubtrairNumeros = () => {
+  const { campoA, campoB } = calcular;
+  return Number(campoA) - Number(campoB);
+}
+
+const getMultiplicarNumeros = () => {
+  const { campoA, campoB } = calcular;
+  return Number(campoA) * Number(campoB);
+}
+
+const getDividirNumeros = () => {
+  const { campoA, campoB } = calcular;
+  return Number(campoA) / Number(campoB);
+}
+
+const getFiltrarCalculos = () => {
+  const {calcularTotal} = calcular;
+
+
+switch (calcularTotal) {
+  case 'somar':
+    return getSomarNumeros();
+  case 'subtrair':
+    return getSubtrairNumeros();
+  case 'multiplicar':
+    return getMultiplicarNumeros();
+  case 'dividir':
+    return getDividirNumeros();
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <header class="p-3 mb-2 mt-4 bg-light rounded-3 ">
+      <h1>Calculadora Aritmética</h1>
+    </header>
+    <form>
+      <input required :value="calcular.campoA" @change="evento => calcular.campoA = evento.target.value" type="number" placeholder="Insira um número">
+      <select @change="evento => calcular.calcularTotal  = evento.target.value" class="m-2">
+        <option value="somar">Somar</option>
+        <option value="subtrair">Subtrair</option>
+        <option value="multiplicar">Multiplicar</option>
+        <option value="dividir">Dividir</option>
+      </select> <br />
+      <input required :value="calcular.campoB" @change="evento => calcular.campoB = evento.target.value" class="mt-4 mb-4" type="number" placeholder="Insira um número">
+      <input placeholder="Total" :value="getFiltrarCalculos()" class="t-campo">
+    </form>
+  </div>
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
+  text-align: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.t-campo {
+  max-width: 100px;
+  margin-left: 6px;
 }
 </style>
